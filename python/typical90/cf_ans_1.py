f3b68a736fc9844846730564bf2decd520d1e232
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-10 08:14:24 +0900

    Typical90 cf with sample algorithm 1

---
+++ python/typical90/cf_ans_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+N = int(sys.stdin.readline())
+s = sys.stdin.readline().rstrip()
+assert len(s) == N
+
+total_comb = N * (N + 1) // 2
+
+combs_to_be_excluded = 0
+
+current_ch = None
+current_count = 0
+
+for ch in s:
+    if current_ch is None:
+        current_ch = ch
+        current_count = 1
+    elif current_ch == ch:
+        current_count += 1
+    else:
+        current_ch = ch
+        combs_to_be_excluded += current_count * (current_count + 1) // 2
+        current_count = 1
+
+combs_to_be_excluded += current_count * (current_count + 1) // 2
+
+ans = total_comb - combs_to_be_excluded
+
+print(ans, end="")
